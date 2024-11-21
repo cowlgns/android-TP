ddd4b6d8ae0029002e1cadc3bb2cb381d29103e7
cowlgns <jhchae1222@gmail.com>
2024-11-21 15:04:02 +0900

    main xml

---
+++ app/src/main/java/com/example/tp03/ShopFragment.kt
@@ -0,0 +1,12 @@
+package com.example.tp03.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tp03.R
+
+class ShopFragment : Fragment(R.layout.fragment_shop) {
+    // 추가적인 동작이나 UI 설정이 필요하면 onViewCreated나 다른 메서드를 사용합니다.
+}
